20416a6be39647ed887848bb8f9f124e60a68bf2
bigmack2304 <37216622+bigmack2304@users.noreply.github.com>
2022-03-30 16:57:20 +0300

    6

--- source/js/menu.js
+++ source/js/menu.js
@@ -5,17 +5,17 @@
 document.addEventListener('DOMContentLoaded', function(evt) {   // если DOM загрузился
 // событие на любой скролл
  //   window.addEventListener('scroll', function(evt) {           // если мы скролим
- //     muse_wheel();                                             // закрываем меню
+ //     muse_wheel();                                             //  
  //   });
 
   // событие на любой клик
- // window.addEventListener('click', function(evt) {           // если мы скролим
- //   click(evt);                                             // закрываем меню
- // });
+  window.addEventListener('click', function(evt) {                // если мы кликнем
+    f_click(evt);                                                 //  
+  });
 });
 
-const object_menu_btn = document.getElementsByClassName("menu__buton");
-for(let i=0; i < object_menu_btn.length; i++) {                                // обходим все найденные меню
+  const object_menu_btn = document.getElementsByClassName("menu__buton");
+  for(let i=0; i < object_menu_btn.length; i++) {                                // обходим все найденные меню
     object_menu_btn[i].addEventListener('click', function (evt) {              // повесить на них обработчик "клик мышкой"
       menu_on_click(evt)
     }); 
@@ -29,7 +29,7 @@ for(let i=0; i < object_menu_btn.length; i++) {                                /
     let object_btn = DOM_obj.currentTarget;                
     let object_elements = DOM_obj.currentTarget.parentElement.parentElement.children[1];
     let object_menu = DOM_obj.currentTarget.parentElement.parentElement;
-    object_menu.classList.toggle("menu--active");
+    object_menu.classList.toggle("js-menu--active");
     object_btn.classList.toggle("menu__buton--active");
     object_elements.classList.toggle("menu__elements--active");
   }
@@ -38,12 +38,42 @@ for(let i=0; i < object_menu_btn.length; i++) {                                /
     close_menu();
   }
 
+  const accept_classes = [  "menu__input",
+                            "menu__buton",
+                            "menu__elements",
+                            "menu__element" ]
+
+  function f_click(DOM_obj) {
+    let obj_class = (typeof DOM_obj.srcElement.className === "object") ? (false) : (DOM_obj.srcElement.className);
+    let obj_open_menu = document.getElementsByClassName("js-menu--active");
+    if (DOM_obj && obj_class) {
+      if (obj_open_menu.length >= 1) {
+        let err_n = 0;
+        for (let i = 0; i < accept_classes.length; i++) {
+          if (obj_class.indexOf(accept_classes[i]) == -1) {
+            err_n++;
+            continue;
+          }
+          break;
+        }
+        console.log(err_n);
+        if (err_n == accept_classes.length) {
+          close_menu();
+        }
+      }
+    } else {
+      if (obj_open_menu.length >= 1) { 
+        close_menu(); 
+      }
+    }
+  }
+
   function close_menu() {
     if (object_menu_btn.length != null) {
       for (let i=0; i < object_menu_btn.length; i++) {
           object_menu_btn[i].classList.remove("menu__buton--active");
           object_menu_btn[i].parentElement.parentElement.children[1].classList.remove("menu__elements--active");
-          object_menu_btn[i].parentElement.parentElement.classList.remove("menu--active");
+          object_menu_btn[i].parentElement.parentElement.classList.remove("js-menu--active");
       }
     } 
   }
